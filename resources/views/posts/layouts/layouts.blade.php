2c46439be634059fc2436e2a8027bde243005592
szwedf <0we3252.018431d@ezweb.ne.jp>
2022-05-16 21:37:48 +0900

    set up bootstrap4

---
+++ resources/views/posts/layouts/layouts.blade.php
@@ -0,0 +1,15 @@
+<html>
+   <head>
+       <title>@yield('title')</title>
+       <meta name="csrf-token" content="{{ csrf_token() }}">
+       <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+         <div class="container">
+            @yield('content')
+         </div>
+        
+
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
