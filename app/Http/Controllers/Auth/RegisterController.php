18a218bb5276b892cc2e3124515b21303882cc68
szwedf <0we3252.018431d@ezweb.ne.jp>
2022-06-19 11:04:02 +0900

    05 create login system

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/email/verify';
 
     /**
      * Create a new controller instance.
@@ -70,3 +70,5 @@ class RegisterController extends Controller
         ]);
     }
 }
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,8 @@ Route::get('/', function () {
 });
 
   Route::resource('products', 'ProductController');
+    Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 ?>
